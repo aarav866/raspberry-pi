04a580fc62d64f6d1c8ef4a4476ec96e09b0d6ae
Your Name <aarav.aggarwal@gmail.com>
2025-09-28 15:18:45 +0530

    line follower

--- Line_follower_robot.py
+++ Line_follower_robot.py
@@ -1,11 +1,34 @@
 from gpiozero import Motor
 from time import sleep
+from gpiozero import DigitalInputDevice
+from signal import pause
 
+# Connect OUT pin of IR sensor to GPIO17
+ir = DigitalInputDevice(27)
+ir2 = DigitalInputDevice(21)
 motor = Motor(forward=2, backward=3)
 motor2 = Motor(forward=4, backward=17)
-
-while True:
+def Forward ():
     motor.forward()
-    sleep(5)
     motor2.forward()
-    sleep(5)
+def Backward ():
+    motor.backward()
+    motor2.backward()
+def Left ():
+    motor2.forward()
+    motor.backward()
+def Right ():
+    motor2.backward()
+    motor.forward()
+    
+while True:
+    if ir2.value == 1 and ir.value==0:
+        Left()
+    elif ir2.value == 0 and ir.value==1:
+        Right()
+    elif ir2.value == 0 and ir.value==0:
+        Forward()
+    elif ir.value == 1 and ir.value == 1:
+        motor.stop()
+        motor2.stop()
+
